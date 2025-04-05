8c8a8fec726ae990a77b881f5727186870f23cec
testgithubsonika <tomarsonika01@gmail.com>
2025-04-05 23:23:58 +0530

    created required pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import Search from './pages/Search';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+
 export default function App() {
   return (
-    <h1 className="text-3xl shadow bg-slate-100 font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/project' element={<Projects />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+        </BrowserRouter>
   )
 }
